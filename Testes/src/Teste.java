e15685280a1e3c977ac8ebe24aa8ad7401fcb993
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-01-23 22:06:34 -0300

    Teste 2

--- Testes/src/Teste.java
+++ Testes/src/Teste.java
@@ -1,18 +1,15 @@
-  /*
-   * Não compila. Falta um ponto-e-vírgula após a impressão, na linha 6.
-   * Isso é normal. Durante a prova, você precisará não apenas analisar o comportamento do 
-   * código, também precisará se atentar as pequenas regras necessárias para que o código 
-   * compile. Para facilitar, veja antes se existe alguma alternativa indicando que o código 
-   * não compila, caso ela exista, verifique cada ponto-e-vírgula, visibilidade, escopo de 
-   * variáveis, nomes e parâmetros de métodos, etc.
-   */
-  
-  class Teste{
+/*
+ * A resposta correta é: Compila e roda, imprimindo de 0 até 19 e depois 15. Repare que a
+ * variável i declarada no for, só é visível dentro do for, por isso podemos declará-la 
+ * novamente logo em seguida.
+ */
 
-    static int id = 1;
-  
-    public static void main(String[] args){
-      System.out.println(id)
-      id = id + 1;
+class Teste {
+    public static void main(String[] args) {
+        for (int i = 0; i < 20; i++) {
+            System.out.println(i);
+        }
+        int i = 15;
+        System.out.println(i);
     }
-  }
+}
